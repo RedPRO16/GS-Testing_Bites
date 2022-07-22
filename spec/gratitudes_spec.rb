166d0324db69a82ec81e382a5e511405d2971c1d
Nesho RADEV <40929562+RedPRO16@users.noreply.github.com>
2022-07-22 16:24:07 +0100

    Updated

--- spec/gratitudes_spec.rb
+++ spec/gratitudes_spec.rb
@@ -1,9 +1,17 @@
 require "gratitudes"
 
 RSpec.describe "Gratitudes" do
-  it "is given appreciation" do
+  it "list of gratitues" do
     gratitudes = Gratitudes.new
     expect(gratitudes.format).to eq "Be grateful for: "
+  end
 
+  it "adding more array of gratitues" do
+    gratitudes = Gratitudes.new
+    gratitudes.add("Happines")
+    gratitudes.add("Health")
+    gratitudes.add("Friends")
+    gratitudes.add("Family")
+    expect(gratitudes.format).to eq "Be grateful for: Happines, Health, Friends, Family"
   end
 end
